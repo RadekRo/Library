fdc9a5bcf302e91d3d2ab084865ab83303b8e0c8
Radek Rocławski <r.roclawski@ankara.pl>
2023-09-26 17:10:44 +0200

    Updated Student

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library.Models
+﻿namespace Library.Models
 {
-    internal class Student
+    public class Student : User
     {
+        public Student(string name) : base(name)
+        {
+            
+        }
     }
 }
